a09b5ab7ce071ac0888b8f4ad6fdce1efc609230
mfrfrabbi <mdfazlerabbi.diu@gmail.com>
2021-04-18 15:33:58 +0600

    initial commit

--- app/src/main/java/com/example/cardview/MainActivity.java
+++ app/src/main/java/com/example/cardview/MainActivity.java
@@ -3,15 +3,47 @@ package com.example.cardview;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    private CardView cardView;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private CardView schoolCardView,hospitalCardView,airportCardView,restaurantCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        schoolCardView = findViewById(R.id.schoolCardViewId);
+        hospitalCardView = findViewById(R.id.hospitalCardViewId);
+        airportCardView = findViewById(R.id.airportCardViewId);
+        restaurantCardView = findViewById(R.id.restaurantCardViewId);
+
+        schoolCardView.setOnClickListener(this);
+        hospitalCardView.setOnClickListener(this);
+        airportCardView.setOnClickListener(this);
+        restaurantCardView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.schoolCardViewId){
+            Intent intent = new Intent(getBaseContext(),SchoolActivity.class);
+            startActivity(intent);
+        }
+        if(v.getId() == R.id.hospitalCardViewId){
+            Intent intent = new Intent(getBaseContext(),HospitalActivity.class);
+            startActivity(intent);
+        }
+        if(v.getId() == R.id.airportCardViewId){
+            Intent intent = new Intent(getBaseContext(),AirportActivity.class);
+            startActivity(intent);
+        }
+        if(v.getId() == R.id.restaurantCardViewId){
+            Intent intent = new Intent(getBaseContext(),RestaurantActivity.class);
+            startActivity(intent);
+        }
 
     }
 }
