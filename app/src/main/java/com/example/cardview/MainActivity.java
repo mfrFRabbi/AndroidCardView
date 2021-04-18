b27692071734efa055441213bbf6b0fc2dca6cc5
mfrfrabbi <mdfazlerabbi.diu@gmail.com>
2021-04-18 11:21:41 +0600

    initial commit

--- app/src/main/java/com/example/cardview/MainActivity.java
+++ app/src/main/java/com/example/cardview/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.cardview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
